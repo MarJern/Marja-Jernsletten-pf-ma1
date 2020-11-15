62a485b09d38a49463b2ef818e30e1750cc5e907
Marja Jernsletten <Marja_jernsletten>
2020-11-15 11:22:02 +0100

    pw-ma1

---
+++ script.js
@@ -0,0 +1,33 @@
+// question 1
+
+// var firstVar;
+
+// question 2
+
+// var name = "marja";
+
+// question 3
+
+// var numbervalue = 30;
+
+// question 4
+
+// var division = 20 / 5;
+
+// question 5
+
+// typeof("frog");
+
+// question 6
+
+// var orderHasShipped = true;
+
+// question 7
+
+// var orderHasShipped = true;
+// if(orderHasShipped === true) { console.log(true) }
+// else { console.log(false) }
+
+// question 8
+
+// for ( var i = 0; i < 9; i++) { console.log(i)}
